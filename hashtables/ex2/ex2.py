8010bb3ec361bc80b85134c3d9554edc732ebedf
Julie Antonio <julieantonio38@gmail.com>
2020-02-14 16:19:10 -0500

    mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,20 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+# create pairs
+# find starting point
+# iterate through and find next ticket
+# return results
 
-    pass
+    #source: destination
+    for i in range(0, length):
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+
+    #set first one to ticket with no destination, assuming that's the starting point????
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+
+    for i in range(1, length):
+        last_source = route[i-1]
+        route[i] = hash_table_retrieve(hashtable, last_source)
+    #slice off last element since it will always be NONE
+    return route[:length -1]
